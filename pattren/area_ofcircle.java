71819590959632059802a3c2d2416452fb2d3f9e
SachinJangir8 <sachinjangir1319@gmail.com>
2026-04-07 22:23:22 +0530

    day 3

---
+++ pattren/area_ofcircle.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class area_ofcircle {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter radius: ");
+        double r = sc.nextDouble();
+
+        double area = Math.PI * r * r;
+
+        System.out.println("Area of Circle = " + area);
+    }
+}
